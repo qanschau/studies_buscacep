e09c7f13073dee2e902ec3fbdbda6817ab1df21d
Querino Anschau Junior <querinojunior@gmail.com>
2023-07-31 23:10:10 -0300

    First Commit

---
+++ src/com/anschaucorp/studies_buscacep/main/BuscaCep.java
@@ -0,0 +1,7 @@
+package com.anschaucorp.studies_buscacep.main;
+
+public class BuscaCep {
+    public static void main(String[] args) {
+        System.out.println("############## **** >> WELCOME TO BUSCA CEP << **** ##############");
+    }
+}
